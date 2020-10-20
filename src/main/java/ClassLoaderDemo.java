0365c0ba3d33d899afbd8d6678fe2dcd1515aeb9
shanmingda <shanmingda@yunshuxie.com>
2020-10-20 22:44:48 +0800

    update ClassLoader

--- src/main/java/ClassLoaderDemo.java
+++ src/main/java/ClassLoaderDemo.java
@@ -4,21 +4,23 @@
  * @author shanmingda
  * @date 2020-09-24 18:46
  */
-public class ClassLoaderDemo {
+public class  ClassLoaderDemo {
 
     public static void main(String[] args) {
         Object o = new Object();
 
         // 获取类加载器
-        System.out.println(o.getClass().getClassLoader());
+//        System.out.println(o.getClass().getClassLoader());
         //null
 
         ClassLoaderDemo demo = new ClassLoaderDemo();
 
 
 //        sun.misc.Launcher$AppClassLoader@18b4aac2
-        System.out.println(demo.getClass().getClassLoader().getParent());
+        System.out.println(demo.getClass().getClassLoader());
 //        sun.misc.Launcher$ExtClassLoader@61bbe9ba
+        System.out.println(demo.getClass().getClassLoader().getParent());
+        // null
         System.out.println(demo.getClass().getClassLoader().getParent().getParent());
 //        null
 
